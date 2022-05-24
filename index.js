f52665ddb47fe88a51ab225a6dfa7d71291a01ab
srsohan284 <srsohan284.com>
2022-05-24 17:59:42 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+const port = process.env.PORT || 5000 ;
+const { MongoClient, ServerApiVersion } = require('mongodb');
+
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.e1zau.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+console.log(uri)
+
+async function run(){
+    try{
+        await client.connect();
+        const partsCollection = client.db('moto_parts').collection('parts');
+        app.get('/parts',async(req,res)=>{
+            const query = {};
+            const cursor = partsCollection.find(query);
+            const parts = await cursor.toArray();
+            res.send(parts);
+        })
+
+    }
+    finally{
+
+    }
+}
+run().catch(console.dir);
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+  })
+
+  app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+  })
